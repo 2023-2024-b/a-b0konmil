b7556c03c6ee740a195dd9917a83591b2b3d27f2
Kónya Milán <b0konmil@vasvari.org>
2024-11-22 10:33:48 +0100

    ConfigureCors

--- PipeLine/PipeLine/Extension/KretaBackendExtension.cs
+++ PipeLine/PipeLine/Extension/KretaBackendExtension.cs
@@ -4,11 +4,20 @@
     {
         public static void AddBackend(this IServiceCollection services)
         {
-
+            services.ConfigureCors();
         }
-        private static void ConfigureCors(IServiceCollection services)
+        private static void ConfigureCors(this IServiceCollection services)
         {
-
+            services.AddCors(option =>
+                 option.AddPolicy(name: "KretaCors",
+                     policy =>
+                     {
+                         policy.WithOrigins("https://0.0.0.0:7020/")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+                     }
+                 )
+            );
         }
     }
 }
